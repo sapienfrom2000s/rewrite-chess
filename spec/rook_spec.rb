8f17745d434bd7041aeb888a0d2807fa5a0ee30c
brickwall <sk292207@gmail.com>
2023-01-26 20:55:26 +0530

    Test bishop

--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../lib/pieces/rook'
+require_relative '../lib/pieces/bishop'
 require_relative '../lib/board'
 require_relative '../lib/setup'
 require_relative '../lib/squares_finder'
@@ -44,6 +45,49 @@ describe Rook do
             setup.deploy(rook2,[[6,6]])
             squares_finder = Squares_Finder.new(board)
             expect(squares_finder.potential_squares([5,6])).to eq([[6,6]])
+        end
+    end
+end
+
+describe Bishop do
+    let(:board) { Board.new }
+    let(:setup) { Setup.new(board) }
+
+    context 'When bishop is at square [1,1]' do
+        it 'will give an array of squares it can go to' do
+            bishop = Bishop.new(:white)
+            setup.deploy(bishop,[[1,1]])
+            squares_finder = Squares_Finder.new(board)
+            expect(squares_finder.potential_squares([1,1])).to eq([[2,2],[3,3],[4,4],[5,5],[6,6],[7,7],[8,8]])
+        end
+    end
+
+    context 'When bishop is at square [5,6]' do
+        it 'will give an array of squares it can go to' do
+            bishop = Bishop.new(:white)
+            setup.deploy(bishop,[[5,6]])
+            squares_finder = Squares_Finder.new(board)
+            expect(squares_finder.potential_squares([5,6])).to eq([[6,7],[7,8],[4,7],[3,8],[4,5],[3,4],[2,3],[1,2],[6,5],[7,4],[8,3]])
+        end
+    end
+
+    context 'When bishop is at square [5,6] and a same side bishop obstructs the path by three sides' do
+        it 'will give an array of squares it can go to' do
+            bishop = Bishop.new(:white)
+            setup.deploy(bishop,[[5,6],[6,7],[4,7],[4,5]])
+            squares_finder = Squares_Finder.new(board)
+            expect(squares_finder.potential_squares([5,6])).to eq([[6,5],[7,4],[8,3]])
+        end
+    end
+
+    context 'When bishop is at square [5,6] and a same side bishop obstructs the path and opp side bishop obstructs path at [6,7]' do
+        it 'will give an array of squares it can go to' do
+            rook1 = Bishop.new(:white)
+            rook2 = Bishop.new(:black)
+            setup.deploy(rook1,[[5,6],[4,7],[4,5],[6,5]])
+            setup.deploy(rook2,[[6,7]])
+            squares_finder = Squares_Finder.new(board)
+            expect(squares_finder.potential_squares([5,6])).to eq([[6,7]])
         end
     end
 end
